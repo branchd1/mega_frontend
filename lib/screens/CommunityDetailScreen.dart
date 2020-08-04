f733ba53e49a8c59e22763280bf6a301b223409b
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-08-04 13:40:45 +0100

    new screens

--- lib/screens/CommunityDetailScreen.dart
+++ lib/screens/CommunityDetailScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mega/components/bars/MyAppBars.dart';
 import 'package:mega/components/bars/MyBottomNav.dart';
+import 'package:mega/components/inputs/SearchInput.dart';
+import 'package:mega/components/texts/BigText.dart';
 import 'package:mega/models/CommunityModel.dart';
 
 class CommunityDetailScreen extends StatefulWidget{
@@ -13,11 +15,73 @@ class CommunityDetailScreen extends StatefulWidget{
 }
 
 class _CommunityDetailScreenState extends State<CommunityDetailScreen>{
+//  Future<List<CommunityModel>> communities;
+  String searchVal;
+
+  void onSearch(String val){
+    setState(() {
+      searchVal = val;
+    });
+  }
+
+//  void tapCardCallback(BuildContext context, dynamic item){
+//    Navigator.push(
+//      context,
+//      MaterialPageRoute(builder: (context) => CommunityDetailScreen(community: item,)),
+//    );
+//  }
+
   @override
   Widget build(BuildContext context) {
+//    if (searchVal == null) communities = CommunityAPI.getCommunities(context);
     return Scaffold(
       appBar: MyAppBars.myAppBar3(),
-      body: Container(),
+      body: Padding(
+        child: Column(
+          children: <Widget>[
+            BigText(this.widget.community.name),
+            Align(
+              child: Text(
+                this.widget.community.isAdmin ? 'admin' : 'member',
+                textAlign: TextAlign.left,
+              ),
+              alignment: Alignment.bottomLeft,
+            ),
+            Padding(
+              child: SearchInput(
+                onChangeCallback: onSearch,
+              ),
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+            ),
+//            FutureBuilder<List<CommunityModel>>(
+//              future: communities,
+//              builder: (BuildContext context, AsyncSnapshot<List<CommunityModel>> snapshot) {
+//                Widget _widget;
+//                if(snapshot.hasData){
+//                  _widget = CardGrid(
+//                    list: searchVal == null ?
+//                    snapshot.data : snapshot.data.where((element) => element.name.toLowerCase().contains(searchVal)).toList(),
+//                    addButtonCallback: (){
+//                      Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => AddCommunityScreen()),
+//                      );
+//                    },
+//                    emptyText: 'No communities',
+//                    tapCardCallback: tapCardCallback,
+//                  );
+//                } else if (snapshot.hasError){
+//                  _widget = Text('Error');
+//                } else {
+//                  _widget = CircularProgressIndicator();
+//                }
+//                return _widget;
+//              },
+//            ),
+          ],
+        ),
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      ),
       bottomNavigationBar: MyBottomNav(
           bottomNavActivePage: BottomNavActivePage.home
       )
