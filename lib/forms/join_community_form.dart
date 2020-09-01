0b4a8d90a326bff4c37ba3e49bb3ae2cd0807687
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-01 07:17:45 +0100

    data compliance warning

--- lib/forms/join_community_form.dart
+++ lib/forms/join_community_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mega/components/buttons/my_button.dart';
 import 'package:mega/components/buttons/my_submit_button.dart';
 import 'package:mega/components/inputs/my_text_input.dart';
+import 'package:mega/components/my_dialog.dart';
 import 'package:mega/components/texts/error_text_plain.dart';
 import 'package:mega/screens/home/home_screen.dart';
 import 'package:mega/services/api/community_api.dart';
@@ -24,13 +26,35 @@ class _JoinCommunityFormState extends State<JoinCommunityForm>{
 
   void submit() async {
     if (_formKey.currentState.validate()){
-      // check email
-      bool _res = await CommunityAPI.joinCommunities(context, _communityKeyController.text, setErrorText);
 
-      if(_res) Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen())
+      String _dialogTitle = 'Data compliance';
+
+      String _dialogText = 'By joining this community, you hereby agree to ' +
+          'share any and all personal information given to us with the ' +
+          'community admin(s).\n\n' + 'Click continue to accept or click cancel to decline.';
+
+      Widget _dialogContinueButton = MyButton(
+          buttonText: 'continue',
+          onPressCallback: () async {
+            bool _res = await CommunityAPI.joinCommunities(context, _communityKeyController.text, setErrorText);
+
+            if(_res) Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen())
+            );
+          }
       );
+
+      Widget _dialogCancelButton = MyButton(
+          buttonText: 'cancel',
+          onPressCallback: () async {
+            Navigator.pop(context);
+          }
+      );
+
+      List<Widget> _dialogButtons = [_dialogContinueButton, _dialogCancelButton];
+
+      MyDialog.showMyDialog(context, _dialogTitle, _dialogText, buttons: _dialogButtons);
     }
   }
 
