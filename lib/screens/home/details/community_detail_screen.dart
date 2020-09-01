23d6c7184d992275c5a35dcc6f5f9f1f299c8ad4
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-01 08:34:02 +0100

    community detail screen

--- lib/screens/home/details/community_detail_screen.dart
+++ lib/screens/home/details/community_detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mega/components/bars/my_app_bars.dart';
+import 'package:mega/components/cards/my_card.dart';
 import 'package:mega/components/cards/my_card_grid.dart';
 import 'package:mega/components/inputs/search_input.dart';
 import 'package:mega/components/texts/big_text.dart';
 import 'package:mega/components/texts/error_text_with_icon.dart';
+import 'package:mega/components/texts/main_text.dart';
 import 'package:mega/models/community_model.dart';
 import 'package:mega/models/state_models/current_community_state_model.dart';
 import 'package:mega/models/feature_model.dart';
@@ -26,7 +28,36 @@ class _CommunityDetailScreenState extends State<CommunityDetailScreen>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBars.myAppBar7(context, logoUrl: this.widget.community.picture),
-      body: Container()
+      body: Padding(
+        child: Column(
+          children: <Widget>[
+            BigText(widget.community.name + ' details'),
+            Align(
+              child: Row(
+                children: <Widget>[
+                  MyCard(imageUrl: widget.community.picture, texts: [],),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    child: Column(
+                      children: <Widget>[
+                        MainText(widget.community.name),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
+                  ),
+                ],
+              ),
+              alignment: Alignment.bottomLeft,
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+              child: Text(widget.community.description),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      ),
     );
   }
 }
