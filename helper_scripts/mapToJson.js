7b9ffca313f2ab0e541e2fe60f5249022a6b7aec
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-01 07:06:09 +0100

    helper scripts

---
+++ helper_scripts/mapToJson.js
@@ -0,0 +1,160 @@
+const fs = require('fs') 
+
+const map = {
+    'admin': {
+      'home': {
+        'metadata': {
+          'show_back_button': 'false'
+        },
+        'components': [
+          {
+            'grid': {
+              'action': {
+                'action_type': 'get',
+                'tag': 'menu',
+                'add_page': {
+                  'new_page': 'add_menu'
+                },
+                'search': {
+                  'field': 'item_name'
+                }
+              },
+              'title': {
+                'value': 'mega\$action\$value.item_name',
+                'prefix': 'item: '
+              },
+              'subtitle': {
+                'value': 'mega\$action\$value.item_price',
+                'prefix': 'price: \$'
+              },
+              'image': {
+                'value': 'mega\$action\$value.item_picture'
+              },
+              'empty_text': {
+                'value': 'No items'
+              },
+              'empty_subtext': {
+                'value': 'add below'
+              },
+              'error_text': {
+                'value': 'Cannot retrieve communities'
+              },
+              'error_subtext': {
+                'value': 'Try again'
+              },
+            }
+          },
+        ]
+      },
+      'add_menu': {
+        'components': [
+          {
+            'form': {
+              'action': {
+                'action_type': 'save',
+                'multipart': [
+                  {
+                    'field': 'item_picture'
+                  }
+                ],
+                'method': 'post',
+                'access': 'community',
+                'tag': 'menu'
+              },
+              'body': [
+                {
+                  'stuffing': {
+                    'height': '20',
+                  }
+                },
+                {
+                  'input': {
+                    'type': 'text',
+                    'name': 'item_name',
+                    'hint': 'item name *',
+                    'validators': {
+                      'required':'',
+                    }
+                  }
+                },
+                {
+                  'stuffing': {
+                    'height': '20',
+                  }
+                },
+                {
+                  'input': {
+                    'type': 'text',
+                    'name': 'item_price',
+                    'hint': 'item price *',
+                    'validators': {
+                      'required':'',
+                      'number': ''
+                    }
+                  }
+                },
+                {
+                  'stuffing': {
+                    'height': '20',
+                  }
+                },
+                {
+                  'input': {
+                    'type': 'file',
+                    'name': 'item_picture',
+                    'hint': 'item picture *',
+                    'validators': {
+                      'required':'',
+                      'max_file_size': '2.0'
+                    }
+                  }
+                },
+                {
+                  'submit_button': {
+                    'value': 'submit',
+                    'action': {
+                      'action_type': 'change_page',
+                      'new_page': 'home', // specify name of new page
+                    }
+                  }
+                },
+              ]
+            }
+          },
+        ]
+      },
+    },
+    'member': {
+      'home': {
+        'components': [
+          {
+            'list': {
+              'action': {
+                'action_type': 'get',
+                'tag': 'menu',
+              },
+              'title': {
+                'value': 'mega\$action\$value.item_name'
+              },
+              'subtitle': {
+                'value': 'mega\$action\$value.item_price',
+                'prefix': 'price: \$'
+              },
+              'empty_text': {
+                'value': 'No items'
+              },
+            }
+          },
+        ]
+      }
+    }
+  }
+  
+fs.writeFile('json.txt', JSON.stringify(map), (err) => { 
+      
+    // In case of a error throw err. 
+    if (err) throw err; 
+})
+  
+  
+  
