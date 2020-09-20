e5f1cc388228c7965741d5afbea2f13eaa9b66a5
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-20 17:52:48 +0100

    started tests and refactoring

--- lib/components/my_dialog.dart
+++ lib/components/my_dialog.dart
@@ -1,25 +1,48 @@
 
 import 'package:flutter/material.dart';
-import 'package:mega/components/buttons/my_button.dart';
 
-class MyDialog{
+/// A dialog used for popups
+class MyDialog extends StatelessWidget{
+
+  /// The main title of the dialog
+  final String title;
+
+  /// The subtitle of the dialog
+  final String subtitle;
+
+  /// Button widgets on the dialog
+  final List<Widget> buttons;
+
+  /// Constructor
+  const MyDialog({Key key, this.title, this.subtitle, this.buttons}) : super(key: key);
+
+  /// Shows the desired dialog on the specified scaffold context
   static void showMyDialog(BuildContext context, String text, String subtext, {List<Widget> buttons}){
     showDialog<void>(
       context: context,
-      barrierDismissible: true,
+      barrierDismissible: true, // dismiss when user clicks outside the dialog
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: SelectableText(text),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                SelectableText(subtext),
-              ],
-            ),
-          ),
-          actions: buttons,
+        return MyDialog(
+          title: text,
+          subtitle: subtext,
+          buttons: buttons
         );
       },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: SelectableText(title),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            SelectableText(subtitle),
+          ],
+        ),
+      ),
+      actions: buttons,
     );
   }
 }
